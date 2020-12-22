1c88f218678e9ebeaacd641381a8f8d1e474b934
Lukas Pietzschmann <lukaspie00@gmail.com>
2020-12-22 21:15:20 +0100

    Added App Icon

--- src/main/kotlin/ui/app/MainApp.kt
+++ src/main/kotlin/ui/app/MainApp.kt
@@ -2,10 +2,12 @@ package ui.app
 
 import javafx.stage.Stage
 import tornadofx.App
+import tornadofx.addStageIcon
 import ui.view.MainView
 
 class MainApp : App(MainView::class, Styles::class) {
 	override fun start(stage: Stage) {
+		addStageIcon(resources.image("/logo.png"))
 		with(stage) {
 			width = 1200.0
 			height = 600.0
